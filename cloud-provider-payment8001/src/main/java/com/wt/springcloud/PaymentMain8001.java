57fd45b038d440d1b223eb2ba0f2fbf9caa040cf
wutong <wt01278@163.com>
2020-05-14 10:25:43 +0800

    commit0514

---
+++ cloud-provider-payment8001/src/main/java/com/wt/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.wt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * @date 2020-05-14 上午 10:11
+ * description
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
