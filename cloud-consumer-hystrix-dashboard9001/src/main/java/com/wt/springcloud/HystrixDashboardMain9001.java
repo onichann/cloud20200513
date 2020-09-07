404227e452e4790d293d602527e777a754cda3b6
wutong <wt01278@163.com>
2020-09-07 14:39:38 +0800

    hystrix

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/wt/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.wt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author wutong
+ * @date 2020/9/7 11:02
+ * introduction
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
