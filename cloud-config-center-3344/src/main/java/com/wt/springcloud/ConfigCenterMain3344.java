0dc0f37c639d0b052b2e6e7c61c607c862cf35af
wutong <wt01278@163.com>
2020-10-09 11:57:20 +0800

    tag ConfigCenter

---
+++ cloud-config-center-3344/src/main/java/com/wt/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.wt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wutong
+ * @date 2020/10/9 10:45
+ * introduction
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
