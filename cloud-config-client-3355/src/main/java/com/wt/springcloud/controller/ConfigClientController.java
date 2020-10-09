626a1d071ad2569488c450197f941cf996c5de62
wutong <wt01278@163.com>
2020-10-09 17:38:02 +0800

    tag ConfigCenter client 手动刷新

--- cloud-config-client-3355/src/main/java/com/wt/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/wt/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.wt.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
  * introduction
  */
 @RestController
+@RefreshScope
+/**
+ * curl -X POST "http://localhost:3355/actuator/refresh"
+ */
 public class ConfigClientController {
 
     @Value("${config.info}")
