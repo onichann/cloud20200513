9eba476cafe66cbe7166fdffe0b8da4516c3e138
wutong <wt01278@163.com>
2020-10-19 17:22:53 +0800

    tag ConfigCenter client rabbitmq刷新

---
+++ cloud-config-client-3366/src/main/java/com/wt/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.wt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wutong
+ * @date 2020/10/19 17:17
+ * introduction
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
