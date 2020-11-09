3e540c4916d3c7fba5cac92c986b87f962d2da01
wutong <wt01278@163.com>
2020-11-10 00:25:06 +0800

    stream rabbitmq

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/wt/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.wt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wutong
+ * @date 2020/11/9 18:25
+ * introduction
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
--- cloud-stream-rabbitmq-consumer8803/src/main/java/com/wt/springcloud/controller/ReceiveMessageListenerController.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/wt/springcloud/controller/ReceiveMessageListenerController.java
@@ -20,6 +20,6 @@ public class ReceiveMessageListenerController {
     @StreamListener(Sink.INPUT)
     public void input(Message<String> message) {
         System.out.println(message.getHeaders());
-        System.out.println("消费者1号,----->接受到的消息: "+message.getPayload()+"\t  port: "+serverPort);
+        System.out.println("消费者2号,----->接受到的消息: "+message.getPayload()+"\t  port: "+serverPort);
     }
 }
