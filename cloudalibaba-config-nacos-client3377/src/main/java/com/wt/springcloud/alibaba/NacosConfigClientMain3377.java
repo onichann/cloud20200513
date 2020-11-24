cad7a10177f70d530474ef604980c704dfd59649
wutong <wt01278@163.com>
2020-11-24 15:35:17 +0800

    cloud alibaba nacos config

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wt/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.wt.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wutong
+ * @date 2020/11/24 14:47
+ * introduction
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
