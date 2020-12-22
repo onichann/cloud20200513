4491a7350d63cb8b4118803f337f3ddc4ce8a727
wutong <wt01278@163.com>
2020-12-22 17:30:04 +0800

    cloud alibaba sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/wt/springcloud/alibaba/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.wt.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wutong
+ * @date 2020/12/22 15:22
+ * introduction
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/wt/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,28 @@
+package com.wt.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wutong
+ * @date 2020/12/22 15:55
+ * introduction
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        return "------testB";
+    }
+}
