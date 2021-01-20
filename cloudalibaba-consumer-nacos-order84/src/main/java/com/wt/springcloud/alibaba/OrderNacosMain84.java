b4bbc6008dd8aafa7ae8812af164d54993258715
wutong <wt01278@163.com>
2021-01-20 23:46:57 +0800

    cloud alibaba sentinel @sentinelresource

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/wt/springcloud/alibaba/OrderNacosMain84.java
@@ -0,0 +1,20 @@
+package com.wt.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author wutong
+ * @date 2021/1/20 23:24
+ * introduction
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class, args);
+    }
+}
